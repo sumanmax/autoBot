61ecf3885545f64e26d11cc9d5bfb5fc3ad70790
sumanmax <Atylishmax1407@gmail.com>
2026-04-21 23:28:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import collections
+# Python 3.10+ এর জন্য এটি প্রয়োজন হতে পারে যদি লাইব্রেরি এরর দেয়
+if not hasattr(collections, 'Iterable'):
+    import collections.abc
+    collections.Iterable = collections.abc.Iterable
+    
 import time
 import json
 import os
