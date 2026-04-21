9f212c213f920a5e7481f0f022869ad9f531275f
sumanmax <Atylishmax1407@gmail.com>
2026-04-21 23:39:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import collections
 if not hasattr(collections, 'Iterable'):
     import collections.abc
     collections.Iterable = collections.abc.Iterable
+    
 import time
 import json
 import os
