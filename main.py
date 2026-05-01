f3f1fa2d34ea6aa295ceb648280097c56791b223
sumanmax <Atylishmax1407@gmail.com>
2026-05-01 17:23:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 # ==========================================
 # ⚙️ CONFIGURATION
 # ==========================================
-BOT_TOKEN = "8734653401:AAG46JyPaLbduKDrj-E9apYUW29asRdZrl0" 
+BOT_TOKEN = "8734653401:AAF0JcvMMAIMq8YWK2-HFfNweOuk12KXZaM" 
 ADMIN_ID = 7852639173  
 SUPPORT_USER = "@mstraders7"
 REG_LINK = "https://broker-qx.pro/sign-up/?lid=2022562"
